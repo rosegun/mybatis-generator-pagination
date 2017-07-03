6eaa8d54e47cf9f5e60580eab947b21cdabdbf7b
qct <quchentao@163.com>
2017-07-03 06:51:09 +0000

    add repository annotation option

--- src/main/java/com/rosegun/plugin/MysqlLimitPlugin.java
+++ src/main/java/com/rosegun/plugin/MysqlLimitPlugin.java
@@ -1,11 +1,13 @@
 package com.rosegun.plugin;
 
 import java.util.List;
+import java.util.Properties;
 import org.mybatis.generator.api.CommentGenerator;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
 import org.mybatis.generator.api.dom.java.Field;
 import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.Interface;
 import org.mybatis.generator.api.dom.java.JavaVisibility;
 import org.mybatis.generator.api.dom.java.Method;
 import org.mybatis.generator.api.dom.java.Parameter;
@@ -13,11 +15,23 @@ import org.mybatis.generator.api.dom.java.TopLevelClass;
 import org.mybatis.generator.api.dom.xml.Attribute;
 import org.mybatis.generator.api.dom.xml.TextElement;
 import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.internal.util.StringUtility;
 
 /**
  * Created by qct on 2017/1/2.
  */
 public class MysqlLimitPlugin extends PluginAdapter {
+
+    private boolean addRepositoryAnnotation = true;
+
+    @Override
+    public void setProperties(Properties properties) {
+        super.setProperties(properties);
+        String addMapperAnnotation = this.properties.getProperty("addRepositoryAnnotation");
+        if (StringUtility.stringHasValue(addMapperAnnotation)) {
+            this.addRepositoryAnnotation = addMapperAnnotation.equalsIgnoreCase("true");
+        }
+    }
 
     @Override
     public boolean modelExampleClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
@@ -26,13 +40,28 @@ public class MysqlLimitPlugin extends PluginAdapter {
         return super.modelExampleClassGenerated(topLevelClass, introspectedTable);
     }
 
+    @Override
+    public boolean sqlMapSelectByExampleWithoutBLOBsElementGenerated(XmlElement element,
+        IntrospectedTable introspectedTable) {
+        XmlElement page = new XmlElement("if");
+        page.addAttribute(new Attribute("test", "page != null"));
+        page.addElement(new TextElement("limit #{page.begin} , #{page.length}"));
+        element.addElement(page);
+        return super.sqlMapSelectByExampleWithoutBLOBsElementGenerated(element, introspectedTable);
+    }
+
+    @Override
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
     private void addPage(TopLevelClass topLevelClass, IntrospectedTable introspectedTable, String name) {
         topLevelClass.addImportedType(new FullyQualifiedJavaType(Page.class.getName()));
-        CommentGenerator commentGenerator = context.getCommentGenerator();
         Field field = new Field();
         field.setVisibility(JavaVisibility.PROTECTED);
         field.setType(new FullyQualifiedJavaType(Page.class.getName()));
         field.setName(name);
+        CommentGenerator commentGenerator = context.getCommentGenerator();
         commentGenerator.addFieldComment(field, introspectedTable);
         topLevelClass.addField(field);
 
@@ -56,17 +85,12 @@ public class MysqlLimitPlugin extends PluginAdapter {
     }
 
     @Override
-    public boolean sqlMapSelectByExampleWithoutBLOBsElementGenerated(XmlElement element,
+    public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass,
         IntrospectedTable introspectedTable) {
-        XmlElement page = new XmlElement("if");
-        page.addAttribute(new Attribute("test", "page != null"));
-        page.addElement(new TextElement("limit #{page.begin} , #{page.length}"));
-        element.addElement(page);
-        return super.sqlMapSelectByExampleWithoutBLOBsElementGenerated(element, introspectedTable);
-    }
-
-    @Override
-    public boolean validate(List<String> list) {
-        return true;
+        if (addRepositoryAnnotation) {
+            interfaze.addImportedType(new FullyQualifiedJavaType("org.springframework.stereotype.Repository"));
+            interfaze.addAnnotation("@Repository");
+        }
+        return super.clientGenerated(interfaze, topLevelClass, introspectedTable);
     }
 }
